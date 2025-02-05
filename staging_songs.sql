2d93e9cf01f3d695823416b3bdc41d3cbdb0cc4d
riccardo <scaringi_riccardo@libero.it>
2025-02-05 17:24:51 +0100

    Create staging_songs.sql

---
+++ staging_songs.sql
@@ -0,0 +1,12 @@
+CREATE TABLE staging_songs (
+    num_songs INTEGER,
+    artist_id VARCHAR,
+    artist_latitude FLOAT,
+    artist_longitude FLOAT,
+    artist_location VARCHAR,
+    artist_name VARCHAR,
+    song_id VARCHAR,
+    title VARCHAR,
+    duration FLOAT,
+    year INTEGER
+);
