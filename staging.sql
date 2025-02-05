f6672287c91bea0885b92739515dda7a2496d8f3
riccardo <scaringi_riccardo@libero.it>
2025-02-05 17:22:53 +0100

    Create staging.sql

---
+++ staging.sql
@@ -0,0 +1,20 @@
+CREATE TABLE staging_events (
+    artist VARCHAR,
+    auth VARCHAR,
+    firstName VARCHAR,
+    gender VARCHAR,
+    itemInSession INTEGER,
+    lastName VARCHAR,
+    length FLOAT,
+    level VARCHAR,
+    location VARCHAR,
+    method VARCHAR,
+    page VARCHAR,
+    registration FLOAT,
+    sessionId INTEGER,
+    song VARCHAR,
+    status INTEGER,
+    ts BIGINT,
+    userAgent VARCHAR,
+    userId INTEGER
+);
