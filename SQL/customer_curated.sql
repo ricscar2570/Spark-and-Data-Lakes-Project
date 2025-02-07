fee3503f0d1a390a8899b488cd801472e2591372
riccardo <scaringi_riccardo@libero.it>
2025-02-07 21:56:42 +0100

    Update customer_curated.sql

--- SQL/customer_curated.sql
+++ SQL/customer_curated.sql
@@ -1,10 +1,14 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS stedi.customer_curated (
-    serialnumber STRING,
-    birthday STRING,
-    registrationdate STRING,
-    sharewithresearchasofdate STRING,
-    lastupdatedate STRING,
-    sharewithfriendsasofdate STRING
+    birthDay STRING,
+    customerName STRING,
+    email STRING,
+    lastUpdateDate STRING, 
+    phone STRING,
+    registrationDate STRING, 
+    serialNumber STRING,
+    shareWithFriendsAsOfDate STRING,
+    shareWithPublicAsOfDate STRING,
+    shareWithResearchAsOfDate TIMESTAMP
 )
 STORED AS PARQUET
 LOCATION 's3://stedi-curated-data/customer_curated/';
